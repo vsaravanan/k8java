ef40ad67aaa13ab5751a3c21d75c141f4eb6dea9
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-06 15:16:18 +0800

    .

--- buildah-deploy.sh
+++ buildah-deploy.sh
@@ -17,13 +17,40 @@ cd ${PROJECT_DIR}
 echo "Maven building JAR..."
 mvn clean package
 
-cd ${BUILD_DIR}
+
+# buildah bud -t k8master:5000/hello-api:latest .
+
+# buildah bud \
+#     -f /data/java/k8java/Dockerfile \
+#     -t k8master:5000/hello-api:latest \
+#     /data/Hello
+
+lxc exec k8master -- mkdir -p "$BUILD_DIR/target/"
+
+read -p "Enter"
+
+lxc file push "$JAR_FILE" k8master/$BUILD_DIR/target/
+
+read -p "Enter"
+
+lxc file push "$BUILD_DIR/Dockerfile" k8master/$BUILD_DIR/
+
+read -p "Enter"
+
 
 echo "Building OCI image..."
+# lxc exec k8master -- buildah bud \
+#     -t ${FULL_IMAGE_NAME} \
+#     -v ${PROJECT_DIR}/target:/workspace/target:ro \
+#     .
+
 lxc exec k8master -- buildah bud \
+    -f ${BUILD_DIR}/Dockerfile \
     -t ${FULL_IMAGE_NAME} \
-    -v ${PROJECT_DIR}/target:/workspace/target:ro \
-    .
+    ${BUILD_DIR}
+
+read -p "Enter"
+
 
 echo "Pushing image..."
 lxc exec k8master -- buildah push \
