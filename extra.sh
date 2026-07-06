a3cc5b510273c462011516656ff6852f73e910e4
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-07-06 14:45:19 +0800

    .

--- extra.sh
+++ extra.sh
@@ -15,3 +15,9 @@ lxc exec k8master -- kubectl apply -f /root/registry.yaml
 
 # Wait for registry pod to be ready
 lxc exec k8master -- kubectl wait --for=condition=ready pod -l app=registry --timeout=300s
+
+lxc exec k8master -- kubectl scale deployment registry --replicas=0
+lxc exec k8master -- kubectl delete deployment registry --ignore-not-found
+lxc exec k8master -- kubectl delete svc registry --ignore-not-found
+lxc exec k8master -- kubectl delete rs -l app=registry --ignore-not-found
+lxc exec k8master -- kubectl delete pods -l app=registry --force --grace-period=0 --ignore-not-found
